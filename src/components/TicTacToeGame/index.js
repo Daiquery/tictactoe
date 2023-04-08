20cc4faa38e47d31c9fecb07e9498053cd75c9e2
Nicholas Burney <nburney@Nicholass-MacBook-Pro.local>
2023-04-07 14:48:15 -1000

    win checker init

--- src/components/TicTacToeGame/index.js
+++ src/components/TicTacToeGame/index.js
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import TTBox from "../TTBox"
 
-const TicTacToeGame = () => {
+const TicTacToeGame = ({p1Name, p2Name}) => {
+    const [currentTurn, setCurrentTurn] = useState(0)
     const [playerTurn, setPlayerTurn] = useState(0);
-    let turnArray = [...Array(9)]
-    console.log(turnArray)
+    const [turnArray, setTurnArray] = useState([...Array(9)]);
    
+    const handleGameTurn = () => {
+       let tempArray = [...turnArray]
+        tempArray[currentTurn] = "X"
+        setTurnArray(tempArray);
+        console.log(turnArray)
+        // checkGameWin()
+        
+        setCurrentTurn(currentTurn + 1);
+        setPlayerTurn(!playerTurn);
+    }
 
     return (
         <div>
             {
                turnArray.map((val, index) => {
-                   return <TTBox playerMark={val} onClick={() => console.log("pizza")} />
+                   return <TTBox key={index} playerMark={val} onClick={() => handleGameTurn()} />
                 })
             }
         </div>
---
+++ src/spc/checkGameWin.js
@@ -0,0 +1,8 @@
+const checkGameWin = () => {
+    // convert the array to string to use regex and do various tests
+    // ex
+    // [X||O],[X||O],[X||O],[],[],[],[],[],[] WIN
+    // [X||O],[X||O],[X||O],[],[],[],[],[],[] WIN
+    // [X||O],[X||O],[X||O],[],[],[],[],[],[] WIN
+
+}
